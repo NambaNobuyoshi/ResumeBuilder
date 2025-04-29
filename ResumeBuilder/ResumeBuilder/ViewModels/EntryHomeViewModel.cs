6efc85abc728aa8ac7b5aeeb5eafb519f18fa718
Nobuyoshi Namba <ramennobu0421@gmail.com>
2025-04-29 15:01:42 +0900

    職務経歴書名が編集できるように修正

--- ResumeBuilder/ResumeBuilder/ViewModels/EntryHomeViewModel.cs
+++ ResumeBuilder/ResumeBuilder/ViewModels/EntryHomeViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
 using System.Windows.Input;
 using ResumeBuilder.Infrastructure;
 using ResumeBuilder.Models;
@@ -22,12 +24,29 @@ namespace ResumeBuilder.ViewModels
                 LastModified = DateTime.Now
             };
 
+            _editableName = _entry.Name;
+
             EditProfileCommand = new RelayCommand(_ => NavigateToProfile());
             EditCareerCommand = new RelayCommand(_ => NavigateToCareer());
             PreviewCommand = new RelayCommand(
                 _ => NavigateToPreview(),
                 _ => CanPreview());
             BackCommand = new RelayCommand(_ => NavigateBack());
+            SaveNameCommand = new RelayCommand(
+                _ => SaveName(),
+                _ => !string.IsNullOrWhiteSpace(EditableName) && EditableName != _entry.Name);
+        }
+
+        // 職務経歴書名称
+        private string _editableName;
+        public string EditableName
+        {
+            get => _editableName;
+            set
+            {
+                if (SetProperty(ref _editableName, value))
+                    CommandManager.InvalidateRequerySuggested(); // Save ボタン活性/非活性を更新
+            }
         }
 
         // プロフィール
@@ -40,11 +59,30 @@ namespace ResumeBuilder.ViewModels
         public ObservableCollection<QualificationItem> Qualifications => _entry.Qualifications;
         public ObservableCollection<SkillItem> Skills => _entry.Skills;
 
+        public ICommand SaveNameCommand { get; }
         public ICommand EditProfileCommand { get; }
         public ICommand EditCareerCommand { get; }
         public ICommand PreviewCommand { get; }
         public ICommand BackCommand { get; }
 
+        private void SaveName()
+        {
+            _entry.Name = EditableName;
+            _entry.LastModified = DateTime.Now;
+            try
+            {
+                _entry.SaveToFile(AppSettings.ResumeDataDirectory);
+                // 画面側のタイトルなどを更新したい場合
+                OnPropertyChanged(nameof(Name));
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"名称の保存に失敗しました: {ex.Message}",
+                                "保存エラー",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error);
+            }
+        }
         private void NavigateToProfile()
         {
             // プロフィール編集画面へ遷移
